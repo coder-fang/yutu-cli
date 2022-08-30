c8b8fd75ab6dce6ff6b34c0f0e6e556f8a1c683a
coder-fang <1846015350@qq.com>
2022-08-30 15:53:47 +0800

    improve

--- index.js
+++ index.js
@@ -3,9 +3,28 @@
 const program = require('commander')
 const chalk = require('chalk')
 const figlet = require('figlet')
-const { Generator, wrapLoading } = require('./lib/Generator')
+const { wrapLoading } = require('./lib/Generator')
 const { getRepoList } = require('./lib/http')
 const ora = require('ora')
+
+program.name(require('./package.json').commandName);
+// 配置提示信息
+program
+// 监听  --help 执行 
+    .on('--help', () => {
+            // 使用figlet 绘制 Logo
+            const rgb = chalk.rgb(94, 231, 223)
+            console.log('\r' + rgb(figlet.textSync('yutu', {
+                font: 'speed',
+                horizontalLayout: 'default',
+                verticalLayout: 'default',
+                width: 120,
+                whitespaceBreak: true
+            })) + chalk.green('欢迎使用 yutuz cli !\n') + chalk.red('                            ♥') + chalk.rgb(238, 0, 238)(` by 杉菜酱~ https://github.com/coder-fang ©${new Date().getFullYear()}`));
+            // 新增说明信息
+            console.log(`\r\n${chalk.bgBlue("yt")} ${chalk.green(`info`)} ${chalk.cyan(`[Core/cli]`)} @yutuz-cli/core@${require('./package.json').version} `)
+            console.log(`\rRun ${chalk.blue(`yt <command> --help`)} for details usage of given command. \r\n`)
+})
 
 program
 // 定义命令和参数
@@ -21,7 +40,7 @@ program
 program
 // 配置版本号信息
     .version(`v${require('./package.json').version}`)
-    .usage('<command> [option]')
+    .usage('<command> [options]')
 
 // 配置config 命令
 program
@@ -43,49 +62,33 @@ program
         console.log(option);
     })
 
-// 配置提示信息
-program
-// 监听  --help 执行 
-    .on('--help', () => {
-            // 使用figlet 绘制 Logo
-            console.log('\r\n' + figlet.textSync('yutuz cli', {
-                font: 'Ghost',
-                horizontalLayout: 'default',
-                verticalLayout: 'default',
-                width: 180,
-                whitespaceBreak: true
-            }));
-            // 新增说明信息
-            console.log(`\r\nRun ${chalk.blue(`yt <command> --help`)} for details usage of given command. \r\n`)
-})
-
 // 初始化命令行（yt init my_template） 
-program
-    .command('init <template> <app-name>')
-    .description('初始化项目模板')
-    .option('-g, --get <path>', 'get value from option')
-    .option('-s, --set <path> <value>')
-    .option('-d, --delete <path>', 'delete option from config')
-    .action(async(template) => {
-        let message = 'Verifying this templates...'
-        const spinner = ora(message)
-        spinner.start()
-        const repoList = await wrapLoading(getRepoList, null)
-        if (!repoList) return
-        const repos = repoList.map(item => item.name)
-        if(!repos.includes(template)){
-            message = '该项目模板不存在，请输入有效的模板名称！'
-            spinner.fail(chalk.red(message))
-            console.log("\r\nPlease Enter `yt list` to find the names of all valid templates,\r\n\nfrom" + 
-            " which you can select the appropriate project template.\r\n");
-            return;
-        }else {
-            // 下载模板
-            // template
-            message = '项目初始化成功！'
-            spinner.succeed(chalk.blue(message))
-        }
-    })
+// program
+//     .command('init <template> <app-name>')
+//     .description('初始化项目模板')
+//     .option('-g, --get <path>', 'get value from option')
+//     .option('-s, --set <path> <value>')
+//     .option('-d, --delete <path>', 'delete option from config')
+//     .action(async(template) => {
+//         let message = 'Verifying this templates...'
+//         const spinner = ora(message)
+//         spinner.start()
+//         const repoList = await wrapLoading(getRepoList, null)
+//         if (!repoList) return
+//         const repos = repoList.map(item => item.name)
+//         if(!repos.includes(template)){
+//             message = '该项目模板不存在，请输入有效的模板名称！'
+//             spinner.fail(chalk.red(message))
+//             console.log("\r\nPlease Enter `yt list` to find the names of all valid templates,\r\n\nfrom" + 
+//             " which you can select the appropriate project template.\r\n");
+//             return;
+//         }else {
+//             // 下载模板
+//             // template
+//             message = '项目初始化成功！'
+//             spinner.succeed(chalk.blue(message))
+//         }
+//     })
 
 // 查看所有的可用模板（yt list ）
 program
@@ -105,25 +108,14 @@ program
         }
     })
 
-    // const installLib = (path, template_name) => {
+    program
+    .command('g')
+    .description('start add open roc-cli ui')
+    .option('-p, --port <port>', 'Port used for the UI Server')
+    .action((option) => {
+        console.log(option);
+    })
 
-    //     const install_command = config[template_name].install || "npm i"; //安装依赖的命令
-    
-    //     return new Promise((resolve, reject) => {
-    //         const workerProcess = exec( // 安装依赖
-    //             install_command, {
-    //                 cwd: path,
-    //             },
-    //             (err) => {
-    //                 if (err) {
-    //                     console.log(err);
-    //                     reject(err);
-    //                 } else {
-    //                     resolve(null);
-    //                 }
-    //             }
-    //         );
-        // }
 program
     .command('*')
     .description('找不到相应的命令')
